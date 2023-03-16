63a688b8c088b7b12c096768b7269be8906f8ce9
ClaraTJ <clara@claratoloba.com>
2023-03-16 15:41:14 +0100

    feat: update App tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Filters, { IFilter } from './components/Filters/Filters';
+import List from './components/List/List';
+import useApp from './useApp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+	const [filters, setFilters] = useState<IFilter>({});
+	const { events, vocals } = useApp(filters);
+
+	function onFiltersChanged(filters: IFilter) {
+		setFilters({ ...filters });
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Your meetings for today</h1>
+			</header>
+			<main>
+				<Filters onFiltersChange={onFiltersChanged} vocals={vocals} />
+				<List events={events} />
+			</main>
+		</div>
+	);
 }
 
 export default App;
