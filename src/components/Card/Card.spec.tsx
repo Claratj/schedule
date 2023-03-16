50e1eca0b2290971b172246aa1dfc91b6a9d8e97
ClaraTJ <clara@claratoloba.com>
2023-03-16 09:58:40 +0100

    feat: add Card tests

---
+++ src/components/Card/Card.spec.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react';
+import Card, { ICardProps } from './Card';
+
+describe('Card component', () => {
+	it('should render with the correct props and the correct classes', () => {
+		const props: ICardProps = {
+			id: 1,
+			title: 'Event',
+			theme: 'DevOps',
+			vocal: 'Vocal',
+			fromTime: '8',
+			toTime: '10',
+			room: 2,
+			top: 0,
+			height: 200,
+			color: 'purple',
+		};
+
+		render(<Card {...props} />);
+
+		expect(screen.getByText(props.title)).toBeInTheDocument();
+		expect(screen.getByText(props.vocal)).toBeInTheDocument();
+		expect(screen.getByText(props.theme)).toBeInTheDocument();
+		expect(screen.getByTestId('card-event-room')).toHaveClass('Card');
+		expect(screen.getByTestId('card-event-title')).toHaveClass('Card__title');
+		expect(screen.getByTestId('card-event-info')).toHaveClass('Card__info');
+		expect(screen.getByTestId('card-event-room')).toHaveStyle(`top: ${props.top}px; height: ${props.height}px;`);
+		expect(screen.getByTestId('card-event-room')).toHaveClass(`Card--${props.color}`);
+		expect(screen.getByTestId('card-event-tag')).toHaveClass(`Card__tag--${props.color}`);
+	});
+
+	it('should render with an empty name if not passed', () => {
+		const props: ICardProps = {
+			id: 1,
+			title: 'Event',
+			theme: 'Backend',
+			vocal: '',
+			fromTime: '8',
+			toTime: '10',
+			room: 2,
+			top: 0,
+			height: 200,
+			color: 'blue',
+		};
+
+		render(<Card {...props} />);
+
+		expect(screen.getByTestId('card-event-vocal')).toBeEmptyDOMElement();
+	});
+});
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import IEvent from '../../types/IEvent';
 import style from './Card.module.scss';
 
-interface ICardProps extends IEvent {
+export interface ICardProps extends IEvent {
 	top: number;
 	height: number;
 	color: string;
@@ -11,14 +11,24 @@ interface ICardProps extends IEvent {
 export default function Card(props: ICardProps) {
 	return (
 		<div
-			key={`card-event-${props.id}-room-${props.room}`}
+			key={`card-event-id-${props.id}-room-${props.room}`}
 			className={classNames(style.Card, [style[`Card--${props.color}`]])}
 			style={{ top: `${props.top}px`, height: `${props.height}px` }}
+			data-testid="card-event-room"
 		>
-			<h2 className={style.Card__title}>{props.title}</h2>
-			<div className={style.Card__info}>
-				<p className={style.Card__vocal}>{props.vocal}</p>
-				<span className={classNames(style.Card__tag, [style[`Card__tag--${props.color}`]])}>{props.theme}</span>
+			<h2 className={style.Card__title} data-testid="card-event-title">
+				{props.title}
+			</h2>
+			<div className={style.Card__info} data-testid="card-event-info">
+				<p className={style.Card__vocal} data-testid="card-event-vocal">
+					{props.vocal}
+				</p>
+				<span
+					className={classNames(style.Card__tag, [style[`Card__tag--${props.color}`]])}
+					data-testid="card-event-tag"
+				>
+					{props.theme}
+				</span>
 			</div>
 		</div>
 	);
